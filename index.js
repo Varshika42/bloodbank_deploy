85c367c1d85219261f7ce517ab24a23434011ba3
Varshika42 <varshikapalanivel@gmail.com>
2025-02-04 23:19:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const donorRouter = require('./Routes/DonorRoutes');
-const recipientRouter = require('./Routes/RecipientRoutes');
-const hospitalRouter = require('./Routes/HospitalRoutes');
+const donorRouter = require('./Routes/DonorRoutes')
+const recipientRouter = require('./Routes/RecipientRoutes')
+const hospitalRouter = require('./Routes/HospitalRoutes')
 
 
-app.use(express.json());
-
-app.use(donorRouter);
-app.use(recipientRouter);
-app.use(hospitalRouter);
-
+app.use(express.json())
 
 app.listen(5000, () => console.log('Server running on port 5000'));
 
 mongoose.connect('mongodb+srv://varshikapalanivel:Varsh24@mycluster.tz14g.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster')
     .then(() => console.log('Database connected...'))
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err))
+
+app.use(donorRouter);
+app.use(recipientRouter);
+app.use(hospitalRouter);
+
 
 ///app.get('/', (req, res) => {
 ///    res.send('Blood Bank Management System Backend is running...');
